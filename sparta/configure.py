d50accbdfaf6f008c874a2e21bbddf697eb9d243
Importcjj <importcjj@gmail.com>
2015-09-23 19:36:37 +0800

    the response of /base/latest is stronger now

--- sparta/configure.py
+++ sparta/configure.py
@@ -3,8 +3,6 @@ try:
     import sparta.sparta_config as Z
 except ImportError:
     import sparta.setting as Z
-import logging
-from logging.handlers import RotatingFileHandler
 
 
 class Basic:
@@ -23,7 +21,4 @@ class ProdConfigure(Basic):
 
 def create_app_with(app, config):
     app.config.from_object(config)
-    filehandler = RotatingFileHandler('log/kratos.log', mode='a')
-    filehandler.setLevel(logging.NOTSET)
-    app.logger.addHandler(filehandler)
     return app
--- sparta/module/base.py
+++ sparta/module/base.py
@@ -12,6 +12,10 @@ def _total_viewer(cards):
     return sum([card.zbViewer for card in cards])
 
 
+def _top_one(f, type):
+    pass
+
+
 def analyse_one(index=None):
     latest_index_obj = Index.latest()
     last_index = latest_index_obj.id
@@ -21,13 +25,15 @@ def analyse_one(index=None):
         return
 
     cards = Outline.get_by_index(index)
-
     outline = dict()
     outline['created_at'] = datetime.strftime(
         latest_index_obj.created_at, '%Y-%m-%d %H:%M')
     outline['total_viewer'] = float(_total_viewer(cards))
     outline['zbTypes'] = []
-    rooms = db.session.query(Outline.zbType, db.func.sum(Outline.zbViewer)).\
+    rooms = db.session.query(
+        Outline.zbType,
+        db.func.sum(Outline.zbViewer),
+        db.func.count(Outline.id)).\
         filter_by(crawlIndex=index).\
         group_by(Outline.zbType).\
         order_by(db.func.sum(Outline.zbViewer).desc()).\
@@ -35,8 +41,20 @@ def analyse_one(index=None):
     for room in rooms:
         zb = {}
         zb['zbType'] = room[0]
-        zb['zbViewers'] = int(room[1])
-        zb['percent'] = zb['zbViewers'] / outline['total_viewer'] * 100
+        zb['zbViewer'] = int(room[1])
+        zb['rooms'] = room[2]
+        zb['percent'] = float(
+            '{:0.2f}'.format(zb['zbViewer'] / outline['total_viewer'] * 100))
+        top_one = sorted(
+            filter(lambda y: y.zbType == zb['zbType'], cards),
+            key=lambda x: x.zbViewer,
+            reverse=True
+        )[0]
+        zb['top_one'] = {
+            'Title': top_one.zbTitle,
+            'player': top_one.zbPlayer,
+            'viewer': top_one.zbViewer
+        }
         outline['zbTypes'].append(zb)
 
     return outline
